e83ed98532aa8a024fa5b988e99f1fb03051b354
yudhaerian <55540987+yudhaerian@users.noreply.github.com>
2021-06-03 14:09:47 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ ('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ ('You are logged in!') }}
+
+                    <table class="table table-responsive">
+                        <tr>
+                            <th>Username</th>
+                            <th>:</th>
+                            <td>{{ $user -> username}}</td>
+                        </tr>
+                        <tr>
+                            <th>Name</th>
+                            <th>:</th>
+                            <td>{{ $user -> name}}</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <th>:</th>
+                            <td>{{ $user -> email}}</td>
+                        </tr>
+                        <tr>
+                            <th>Created At</th>
+                            <th>:</th>
+                            <td>{{ $user -> created_at}}</td>
+                        </tr>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
